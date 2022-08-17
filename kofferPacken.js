b1c5706b7a2f574fcbc484998ec25d9cae48e2e1
Amna El Sayed <amnaelsayed2@gmail.com>
2022-08-17 13:25:43 +0200

    gradient-string

--- kofferPacken.js
+++ kofferPacken.js
@@ -1,9 +1,11 @@
 import promptSync from "prompt-sync";
-import gradient from "gradient-string";
 import chalk from "chalk";
-
+import gradient from "gradient-string";
+let coolGradient = gradient("red", "green", "blue");
+let coolString = coolGradient("This is a fancy string!");
+console.log(coolString);
 // const gradient = require("gradient-string");
-const prompt = promptSync();
+// const prompt = promptSync();
 
 // let duck = gradient("orange", "yellow").multiline(
 //   ["  __", "<(o )___", " ( ._> /", "  `---'"].join("\n")
