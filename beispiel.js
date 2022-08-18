8c2b05cc2711306b76c8edf02f82fd490a79905d
Amna El Sayed <amnaelsayed2@gmail.com>
2022-08-18 13:34:30 +0200

    nulleinsspiel

--- beispiel.js
+++ beispiel.js
@@ -123,8 +123,48 @@ console.log(iqOne);
 // const insertTimer = timer();
 // console.log(2);
 
-const tomorrowGame = tomorrow();
-var refreshIntervalId = setInterval(tomorrowGame, 10000);
+// const tomorrowGame = tomorrow();
+// var refreshIntervalId = setInterval(tomorrowGame, 10000);
 
-/* later */
-clearInterval(refreshIntervalId);
+// /* later */
+// clearInterval(refreshIntervalId);
+
+2;
+3;
+4;
+5;
+6;
+7;
+8;
+9;
+10;
+11;
+console.log("\nI'm the normal output");
+console.log("\x1b[31mAnd now I'm red!");
+console.log("Shoot, why am I still red?");
+console.log("I need to \x1b[0mreset my console to get back to normal");
+console.log(
+  "Colors \x1b[32mcan \x1b[33mchange \x1b[35min \x1b[36mthe \x1b[34msame \x1b[0mlog"
+);
+console.log(
+  "\x1b[1mBRIGHT colors \x1b[32mare \x1b[33mbolded \x1b[35mand \x1b[36mbrighter \x1b[0m"
+);
+console.log("\x1b[2mDIM colors \x1b[32mare \x1b[33mdarker \x1b[0m");
+console.log(
+  "and of course, \x1b[41mwe have \x1b[30m\x1b[43mbackground colors\x1b[0m"
+);
+console.log(
+  "\x1b[7mReverse \x1b[32mswap \x1b[33mforeground \x1b[35mand \x1b[36mbackground\x1b[0m"
+);
+console.log(
+  "\x1b[8m\x1b[41mthis text \x1b[43mis hidden \x1b[42mbut the background\x1b[42m still comes \x1b[45mthrough\x1b[0m"
+);
+console.log(
+  "\x1b[4mgetting fancy with underlines \x1b[30m\x1b[3m\x1b[105mand italics\x1b[0m"
+);
+
+console.log(
+  "%cStyling is %cfun",
+  "color: #00ff33; font-size: 14px",
+  "text-transform:uppercase; font-size: 40px; color: #ff33dd"
+);
