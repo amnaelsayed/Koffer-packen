32d21db821237d0117de4477dc480e1ca0c392bb
elenosis <eleni.orfanou.88@gmail.com>
2022-08-19 12:48:39 +0200

    what the heck is missing?

--- whatIsMissing.js
+++ whatIsMissing.js
@@ -56,21 +56,82 @@ const whatIsMissingArray = [wim1, wim2];
 
 function whatIsMissing(array, array2) {
   const i = Math.ceil(Math.random() * array.length - 1);
-  console.log(array);
+  const array3 = [];
+  console.log(`You have 20 seconds to memorise the objects :`, `\n`, array);
   setTimeout(function () {
-    let ii = Math.ceil(Math.random() * array.length);
+    // let ii = Math.ceil(Math.random() * array.length-1);
 
     console.clear();
-    array2.push(array.splice(i, 1)[0]);
-    array2[i] = array[ii];
+    const missing = array2.push(array.splice(i, 1)[0]);
+    const missingOne = array;
 
-    console.log();
-    console.log(array2);
-    console.log(array2.pop());
-    // console.log(array);
-  }, 6000);
+    let seconds = new Date().getSeconds();
+    if (seconds <= 7 || seconds >= 56) {
+      array3.push(`a:` + array2.shift());
+      array3.push(`b:` + array2.shift());
+      array3.push(`c:` + array2.pop());
+      array3.push(`d:` + array2.shift());
+      array3.push(`e:` + array2[0]);
+      console.log(array);
+      console.log(`Choose the one that is missing from the top`);
+      console.log(array3);
+    } else if (
+      (seconds > 7 && seconds <= 12) ||
+      (seconds > 48 && seconds < 56)
+    ) {
+      array3.push(`a:` + array2.pop());
+      array3.push(`b:` + array2.pop());
+      array3.push(`c:` + array2.pop());
+      array3.push(`d:` + array2.pop());
+      array3.push(`e:` + array2[0]);
 
-  // array.splice(i, 1);
-  // console.log(array);
+      console.log(array);
+      console.log(`Choose the one that is missing from the top`);
+      console.log(array3);
+    } else if (
+      (seconds > 12 && seconds <= 20) ||
+      (seconds > 39 && seconds < 48)
+    ) {
+      array3.push(`a:` + array2.shift());
+      array3.push(`b:` + array2.shift());
+      array3.push(`c:` + array2.pop());
+      array3.push(`d:` + array2.pop());
+      array3.push(`e:` + array2[0]);
+
+      console.log(array);
+      console.log(`Choose the one that is missing from the top:`);
+      console.log(array3);
+    } else if (
+      (seconds > 20 && seconds <= 23) ||
+      (seconds > 33 && seconds < 39)
+    ) {
+      array3.push(`a:` + array2.shift());
+      array3.push(`b:` + array2.pop());
+      array3.push(`c:` + array2.pop());
+      array3.push(`d:` + array2.pop());
+      array3.push(`e:` + array2[0]);
+
+      console.log(array);
+      console.log(`Choose the one that is missing from the top`);
+      console.log(array3);
+    } else if (
+      (seconds > 23 && seconds <= 30) ||
+      (seconds > 31 && seconds < 33)
+    ) {
+      array3.push(`a:` + array2.shift());
+      array3.push(`b:` + array2.shift());
+      array3.push(`c:` + array2.shift());
+      array3.push(`d:` + array2.pop());
+      array3.push(`e:` + array2[0]);
+
+      console.log(array);
+      console.log(`Choose the one that is missing from the top`);
+      console.log(array3);
+    } else {
+      array3.push(...array2);
+
+      console.log(array3);
+    }
+  }, 10000);
 }
 whatIsMissing(whatIsMissingArray[0], whatIsMissingArray[1]);
